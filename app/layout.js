2f709bf068ec37409ba029435bde3e4ba7ddc3e4
Razan-J-Mohammad <razanmohmj20@gmail.com>
2024-10-30 08:36:21 +0200

    create outerSidebar

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,23 @@
 import Header from './components/Header/Header';
+import OuterSidebar from './components/Sidebar/OuterSidebar';
+import InnerSidebar from './components/Sidebar/InnerSidebar';
 import './globals.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
-
 export default function Layout({ children }) {
-  return (
-    <html lang="en">
-      <body>
+    return (
+        <html lang="en">
+        <body>
         <div className="app-layout">
-          <Header />
+            <Header/>
+            <div className="main-content">
+                <OuterSidebar/>
+                <div className="content-area">
+                    <InnerSidebar/>
+
+                </div>
+            </div>
         </div>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
