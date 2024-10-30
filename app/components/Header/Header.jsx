f8c3c08825db6e1ec4503131fecdd7218a565e25
Razan-J-Mohammad <razanmohmj20@gmail.com>
2024-10-30 17:56:38 +0200

    update Header

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -4,47 +4,47 @@ import styles from './Header.module.css';
 import '../../variables.css'
 
 export default function Header() {
-  return (
-      <header className={styles.header}>
-        <div className={styles.logoContainer}>
-          <Image src="/logo.png" alt="Logo" width={156} height={41} />
-        </div>
+    return (
+        <header className={styles.header}>
+            <div className={styles.logoContainer}>
+                <Image src="/logo.png" alt="Logo" width={156} height={41} />
+            </div>
 
-        <div className={styles.searchContainer}>
-          <i className={`fa fa-magnifying-glass ${styles.searchIcon}`}></i>
-          <input type="text" placeholder="Search" className={styles.searchInput} />
-        </div>
+            <div className={styles.searchContainer}>
+                <i className={`fa fa-magnifying-glass ${styles.searchIcon}`}></i>
+                <input type="text" placeholder="Search" className={styles.searchInput} />
+            </div>
 
-        <div className={styles.actions}>
-          <LanguageSelector />
-          <NotificationIcon />
-          <UserProfile />
-        </div>
-      </header>
-  );
+            <div className={styles.actions}>
+                <LanguageSelector />
+                <NotificationIcon />
+                <UserProfile />
+            </div>
+        </header>
+    );
 }
 
 const LanguageSelector = () => (
     <div className={styles.languageContainer}>
-      <Image src="/us.webp" alt="American Flag" width={24} height={24} className={styles.flagIcon} />
-      <span>ENG</span>
-      <i className={`fa fa-angle-down ${styles.dropdownIcon}`}></i>
+        <Image src="/us.webp" alt="American Flag" width={24} height={24} className={styles.flagIcon} />
+        <span>ENG</span>
+        <i className={`fa fa-angle-down`}></i>
     </div>
 );
 
 const NotificationIcon = () => (
     <div className={styles.notification}>
-      <i className="fa fa-bell"></i>
-      <span className={styles.notificationDot}></span>
+        <i className="fa fa-bell"></i>
+        <span className={styles.notificationDot}></span>
     </div>
 );
 
 const UserProfile = () => (
     <div className={styles.profile}>
-      <Image src="/razan1.jpeg" alt="User Profile" width={40} height={40} className={styles.profileImage} />
-      <div className={styles.profileInfo}>
-        <span className={styles.profileName}>Razan Mohamad</span>
-        <span className={styles.profileTitle}>Full Stack</span>
-      </div>
+        <Image src="/razan1.jpeg" alt="User Profile" width={40} height={40} className={styles.profileImage} />
+        <div className={styles.profileInfo}>
+            <span className={styles.profileName}>Razan Mohamad</span>
+            <span className={styles.profileTitle}>Full Stack</span>
+        </div>
     </div>
 );
--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,20 @@
 import Header from './components/Header/Header';
 import OuterSidebar from './components/Sidebar/OuterSidebar';
-import InnerSidebar from './components/Sidebar/InnerSidebar';
 import './globals.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 export default function Layout({ children }) {
     return (
         <html lang="en">
+        <head>
+            <link rel="icon" href="/public/favicon.ico" />
+            <title>Legislate</title>
+        </head>
         <body>
         <div className="app-layout">
             <Header/>
-            <div className="main-content">
+            <div className="container">
                 <OuterSidebar/>
-                <div className="content-area">
-                    <InnerSidebar/>
 
-                </div>
             </div>
         </div>
         </body>
